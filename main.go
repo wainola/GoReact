390849ebcb795728a2cbb8c4276b2f88d00b4d89
Nicolás Riquelme Guzmán <nrriquelme@gmail.com>
2018-04-26 00:39:33 -0300

    sirviendo react app

--- main.go
+++ main.go
@@ -1,14 +1,23 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"log"
 	"net/http"
 )
 
+func GetApi(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Api endpoint")
+}
+
 func main() {
+	var dir string
+	flag.StringVar(&dir, "dir", ".", "./src")
 	fs := http.FileServer(http.Dir("src"))
 	http.Handle("/", fs)
+	http.HandleFunc("/api", GetApi)
 
-	log.Println("Listening...")
+	log.Println("Listening on port 4500")
 	http.ListenAndServe(":4500", nil)
 }
